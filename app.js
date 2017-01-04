207b900b5e4939ea0ee456411e1fb822306b1c33
JGuiraud <jerome31guiraud@gmail.com>
2017-01-04 16:48:46 +0100

    ex done

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 var species = {
-	cat    : "cat.jpg",
-	bear   : "bear.jpg",
-	fish   : "fish.jpg"
+	cat    : "img/cat.jpg",
+	bear   : "img/bear.jpg",
+	fish   : "img/fish.jpg"
 };
-
-function main(){
 	/* 
 	tout votre code doit se trouver dans cette fonction,
 	vous pouvez biensur créer d'autres fonctions si nécessaire
 	*/
+
+	function main(){
+		$(".ui.button").click(function() {
+			var type = this.getAttribute("data-animal");
+
+			if (type == "cat") {
+				var chat = species['cat'];
+				$("#holder").html('<img src="' + chat +'"/>');
+			} else if (type == "bear") {
+				var ours = species['bear'];
+				$("#holder").html('<img src="' + ours +'"/>');
+			} else {
+				var poisson = species['fish'];
+				$("#holder").html('<img src="' + poisson +'"/>');
+			}
+		});
+	}
+
+	$(document).ready(function(){
+		main();
+	});
 
 	// 1. placez un listener sur le click des éléments <button>
 	// 2. dans le callback récupérez le data attribute animal
@@ -19,9 +38,3 @@ function main(){
 	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
 	
 	
-	 
-}
-
-$(document).ready(function(){
-	main();
-});
